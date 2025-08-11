135041adc718bed176d7dd7c243b4ac3f6d06a11
drbiga <mcost16@lsu.edu>
2025-08-11 11:13:01 -0300

    fix unused react import at main and backend health check protocol issue

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
@@ -6,7 +5,7 @@ import { AuthProvider } from './hooks/auth.tsx'
 import './lib/tabManager.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+  <AuthProvider>
+    <App />
+  </AuthProvider>
 )
